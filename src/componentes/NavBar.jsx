a218c72f83a5e90b005505dbe87d869b31796318
JOHN-Z23 <john23zapata@gmail.com>
2022-08-29 22:24:20 -0300

    Modificando productos

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBarExample = () => {
        <>
        <Navbar className="navBg" variant="dark" expand="lg">
         <Container>
-            <Navbar.Brand as={Link} to="/" >Ferre Global</Navbar.Brand>
+            <Navbar.Brand className="ferre" as={Link} to="/" >Ferre Global</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
